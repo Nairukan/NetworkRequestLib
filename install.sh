3927b71ed0f8b2f9d6cc01aeec4f99d40678abef
Nairukan <koren_nikita2002@mail.ru>
2024-04-16 07:06:28 +0300

    disable DEBUG MODE + repair install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
+sudo rm -rf build/*
 cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Release
 sudo make -C build/
 sudo make -C build/ install
 sudo rm -rf build/*
-
 cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Debug
 sudo make -C build/
 sudo make -C build/ install
-sudo rm -rf build/*
